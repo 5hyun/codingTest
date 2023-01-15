c3ad1ff6d0fc7f5f82b15ce122e65389b36f1418
5hyun <qhslsl@gmail.com>
2023-01-15 21:28:51 +0900

    양궁대회(DFS 활용)

--- 프로그래머스/양궁대회.js
+++ 프로그래머스/양궁대회.js
@@ -1,26 +1,42 @@
 function solution(n, info) {
   var answer = [];
-  let temp = [];
+  let maxPoint = 0;
   let dy = Array.from({ length: 11 }, () => 0);
 
-  function DFS(a, r, now, count, arr) {
-    if (count === n) {
-      if (a < r) temp.push([r - a, arr.slice()]);
+  function DFS(point, count, a_sum, r_sum, ary) {
+    if (n < count) return;
+
+    if (point > 10) {
+      let diff = r_sum - a_sum;
+
+      if (diff > maxPoint) {
+        ary[10] = n - count;
+        maxPoint = diff;
+        answer = ary;
+      }
+      return;
+    }
+
+    // 라이언이 점수 먹음
+    if (n > count) {
+      let temp = ary.slice();
+      temp[10 - point] = info[10 - point] + 1;
+      DFS(point + 1, count + info[10 - point] + 1, a_sum, r_sum + point, temp);
+    }
+
+    //  어피치가 점수 먹음
+    if (info[10 - point] > 0) {
+      DFS(point + 1, count, a_sum + point, r_sum, ary.slice());
     } else {
-      let ary = arr.slice();
-      // 라이언이 안 쏜 경우
-      DFS(info[now] > 0 ? a + now : a, r, now + 1, count, ary);
-      //  라이언이 쏜 경우
-      ary[now] += info[now] + 1;
-      DFS(a, r + now, now + 1, count + info[now] + 1, ary);
+      //  둘 다 못 먹음
+      DFS(point + 1, count, a_sum, r_sum, ary.slice());
     }
   }
 
-  DFS(0, 0, 1, 0, dy);
+  DFS(0, 0, 0, 0, dy.slice());
 
-  console.log(temp);
-
-  return answer;
+  if (answer.length > 0) return answer;
+  else return [-1];
 }
 
 let n = 5;
