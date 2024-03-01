0661625901274c5a7d84659d7410cd14fc6ec8f9
5hyun <qhslsl@gmail.com>
2024-03-01 16:56:24 +0900

    Feat: 에라토스테네스의 체로 다시 풀음

--- baekjoon/소수 구하기.js
+++ baekjoon/소수 구하기.js
@@ -1,32 +1,55 @@
+// function solution(input) {
+//   const temp = input.split(" ");
+//   const m = +temp[0];
+//   const n = +temp[1];
+//   const answer = [];
+//
+//   function isPrime(n) {
+//     if (n === 1) return false;
+//
+//     let flag = true;
+//
+//     for (let i = 2; i <= Math.sqrt(n); i++) {
+//       if (n % i === 0) {
+//         flag = false;
+//         break;
+//       }
+//     }
+//
+//     return flag;
+//   }
+//
+//   for (let i = m; i <= n; i++) {
+//     isPrime(i) && answer.push(i);
+//   }
+//
+//   return answer.join("\n");
+// }
+
 function solution(input) {
-  const temp = input.split(" ");
-  const m = +temp[0];
-  const n = +temp[1];
+  const [m, n] = input.split(" ").map(Number);
   const answer = [];
 
-  function isPrime(n) {
-    if (n === 1) return false;
+  let arr = Array.from({ length: n + 1 }, () => true);
+  arr[0] = false;
+  arr[1] = false;
 
-    let flag = true;
-
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-      if (n % i === 0) {
-        flag = false;
-        break;
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
       }
     }
-
-    return flag;
   }
 
   for (let i = m; i <= n; i++) {
-    isPrime(i) && answer.push(i);
+    if (arr[i]) answer.push(i);
   }
 
   return answer.join("\n");
 }
 
-// const input = "1 7";
+// const input = "3 16";
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString();
 console.log(solution(input));
