f9a80239c55e917940a133dcb9241e5f47d44574
5hyun <86971770+5hyun@users.noreply.github.com>
2025-01-28 22:49:29 +0900

    [level 2] Title: 구명보트, Time: 14.22 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,20 @@
+function solution(people, limit) {
+    var answer = 0;
+    people = people.sort((a,b)=>a-b);
+    
+    let left = 0;
+    let right = people.length-1;
+    
+    while(left <= right) {
+        if (people[left] + people[right] <= limit) {
+            answer++;
+            left++;
+            right--;
+        } else {
+            answer++;
+            right--;
+        }
+    }
+    
+    return answer;
+}
