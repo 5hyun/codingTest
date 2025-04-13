e69807558458287d3c5a609f748a0075da8d4b15
5hyun <86971770+5hyun@users.noreply.github.com>
2025-04-13 19:03:43 +0900

    [level 2] Title: 전화번호 목록, Time: 494.52 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,16 @@
+function solution(phone_book) {
+    var answer = true;
+    
+    phone_book.sort((a,b)=>a.localeCompare(b));
+    
+    for (let i = 0; i < phone_book.length - 1; i++){
+        if (phone_book[i].length < phone_book[i+1].length) {
+            if (phone_book[i+1].slice(0, phone_book[i].length) === phone_book[i]) {
+                answer =false;
+                break;
+            }
+        }
+    }
+    
+    return answer;
+}
