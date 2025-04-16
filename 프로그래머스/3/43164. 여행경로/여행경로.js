77762e00b88d3b44112f371bcf776b304b8ef248
5hyun <86971770+5hyun@users.noreply.github.com>
2025-04-16 23:26:33 +0900

    [level 3] Title: 여행경로, Time: 515.18 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,28 +1,32 @@
 function solution(tickets) {
+    var answer = [];
+    
     const graph = {};
     
-    // 그래프 생성 (각 공항별 도착지 리스트)
-    for (const [start, end] of tickets) {
-        if (!graph[start]) graph[start] = [];
-        graph[start].push(end);
+    tickets.forEach((ticket)=>{
+        const [a, b] = ticket;
+        
+        if (!graph[a]) {
+            graph[a] = [b];
+        } else {
+            graph[a].push(b);
+        }
+    });
+    
+    for (let airport in graph) {
+        graph[airport].sort((a,b)=>b.localeCompare(a));
     }
     
-    // 각 공항의 도착지 알파벳 역순 정렬 (pop() 사용을 위해)
-    for (const airport in graph) {
-        graph[airport].sort((a, b) => b.localeCompare(a));
-    }
-    
-    const path = [];
-    
-    function dfs(airport) {
+    const dfs = (airport) => {
         while (graph[airport] && graph[airport].length > 0) {
             const nextAirport = graph[airport].pop();
             dfs(nextAirport);
         }
-        path.unshift(airport);
+        
+        answer.unshift(airport)
     }
     
-    dfs('ICN');
+    dfs('ICN')
     
-    return path;
+    return answer;
 }
