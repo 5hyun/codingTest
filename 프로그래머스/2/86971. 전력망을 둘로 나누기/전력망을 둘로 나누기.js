7db20c79d7ae0aa9efa255cb924ded0368151d99
5hyun <86971770+5hyun@users.noreply.github.com>
2025-04-16 22:26:47 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 26.01 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -3,21 +3,21 @@ function solution(n, wires) {
     
     const graph = {};
     
-    [...Array(n)].forEach((_,index) => {
-        graph[index+1] = [];
-    })
+    for(let i = 1; i <= n; i++) {
+        graph[i] = [];
+    }
     
-    wires.forEach((wire)=>{
+    wires.forEach((wire) => {
         const [a, b] = wire;
         
         graph[a].push(b);
         graph[b].push(a);
     })
     
-    wires.forEach((wire)=>{
+    wires.forEach((wire)=> {
         const [cut_a, cut_b] = wire;
         let length = 1;
-        const visited = Array.from({length: n+1}, ()=>false);
+        const visited = Array.from({length: n+1}, () => false);
         visited[1] = true;
         
         const queue = [1];
@@ -25,22 +25,22 @@ function solution(n, wires) {
         while(queue.length > 0) {
             const node = queue.shift();
             
-            graph[node].forEach((v)=>{
-                if ((node === cut_a && v === cut_b) || (node === cut_b && v === cut_a)) {
+            graph[node].forEach((g) => {
+                if ((g === cut_a && node === cut_b) || (g === cut_b && node === cut_a)){
                     return;
                 }
                 
-                if (!visited[v]){
-                    queue.push(v);
-                    visited[v] = true;
+                if (!visited[g]) {
+                    visited[g] = true;
+                    queue.push(g);
+                    
                     length++;
                 }
-            })
+            })   
         }
         
-        answer = Math.min(answer, Math.abs(length-(n-length)));
+        answer = Math.min(answer, Math.abs(length - (n - length)));
     })
-    
     
     return answer;
 }
