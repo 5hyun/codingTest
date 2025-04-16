0a71f639cda7ec572e7cd34a2e1a480528c02757
5hyun <86971770+5hyun@users.noreply.github.com>
2025-04-16 20:38:17 +0900

    [level 2] Title: 주식가격, Time: 10.75 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,21 +1,20 @@
 function solution(prices) {
     var answer = Array.from({length: prices.length}, () => 0);
-    let stack = [];
+    
+    const stack = [];
     
     for (let i = 0; i < prices.length; i++) {
-        let now = prices[i];
-        while (stack.length > 0 && prices[stack[stack.length - 1]] > now) {
-            let j = stack.pop();
-            answer[j] = i - j;
+        while(stack.length > 0 && prices[stack[stack.length-1]] > prices[i] ) {
+            const prevIndex = stack.pop()
+            answer[prevIndex] = i-prevIndex;
         }
         
         stack.push(i);
     }
     
-    while(stack.length > 0) {
-        let j = stack.pop();
-        answer[j] = prices.length - 1 -j;
-    }
+    stack.forEach((s)=> {
+        answer[s] = prices.length - s - 1;
+    })
     
     return answer;
 }
